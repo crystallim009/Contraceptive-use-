91b3474c151f0ea1a1ddd06fa845f59d745f3e8a
crystallim009 <64355650+crystallim009@users.noreply.github.com>
2020-07-25 17:28:30 +0800

    Delete contra_pdf_to_csv.py

--- contra_pdf_to_csv.py
+++
@@ -1,6 +0,0 @@
-# convert pdf to csv
-
-import tabula
-df = tabula.read_pdf("databooklet.pdf", encoding='utf-8', pages='17-24', guess=False)
-
-tabula.convert_into("databooklet.pdf", "contra1.csv", output_format="csv", pages='17-24')
